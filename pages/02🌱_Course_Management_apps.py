91488c0757957b12a5359cb27a1e2891d52c18b3
MK <99416359+MK316@users.noreply.github.com>
2025-08-31 23:29:28 +0900

    Update 02🌱_Course_Management_apps.py

--- pages/02🌱_Course_Management_apps.py
+++ pages/02🌱_Course_Management_apps.py
@@ -175,7 +175,7 @@ with tabs[3]:
                 background-color: #002244;
             }
         </style>
-        <a href="https://docs.google.com/spreadsheets/d/1EtXckbP8BtkeoHEqUc0KFv5xarUgegm8rJ-Ri0rloQo/edit?usp=sharing" target="_blank">
+        <a href="https://docs.google.com/spreadsheets/d/1luqWB2qoJ51QNyyLJ6AFUdhJw7d8oDyFsJFmPOsa5FM/edit?usp=sharing" target="_blank">
             <button class="custom-button">🎯 Click: Go to Google Sheet</button>
         </a>
     """
