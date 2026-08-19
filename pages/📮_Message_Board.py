4df4b5a94dce650cf3e001b8119ad3274241d656
MK <99416359+MK316@users.noreply.github.com>
2026-08-20 00:12:21 +0900

    Update 📮_Message_Board.py

--- pages/📮_Message_Board.py
+++ pages/📮_Message_Board.py
@@ -5,7 +5,7 @@ def main():
     st.caption("💙 Greetings! Feel free to leave any feedback, suggestions, or messages about the application on this page. I'll make sure to look into them as soon as I can! 😍")
     st.write("➡️ Click the '+' sign to write.")
     # Padlet embed URL (you need to replace this with your actual Padlet embed URL)
-    padlet_url = "https://padlet.com/mirankim316/apps4u"
+    padlet_url = "https://padlet.com/mirankim316/english_phonology"
 
     # Create an iframe to embed the Padlet
     padlet_iframe = f"<iframe src='{padlet_url}' width='100%' height='600' frameborder='0' allow='autoplay'></iframe>"
